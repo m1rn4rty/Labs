8b4aab3034e88dd78aa135c5253912a53a470392
m1rn4rty <162566458+m1rn4rty@users.noreply.github.com>
2025-05-14 20:07:18 +0300

    Add files via upload
    Signed-off-by: m1rn4rty <162566458+m1rn4rty@users.noreply.github.com>

---
+++ Lab7/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lab7_AllTasks")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab7_AllTasks")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("77777777-7777-7777-7777-777777777777")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
