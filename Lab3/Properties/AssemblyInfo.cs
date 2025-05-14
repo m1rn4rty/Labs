be8d97afbd73cab0ebe8bd4819c894479769aba0
m1rn4rty <162566458+m1rn4rty@users.noreply.github.com>
2025-05-14 20:00:55 +0300

    Add files via upload
    Signed-off-by: m1rn4rty <162566458+m1rn4rty@users.noreply.github.com>

---
+++ Lab3/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lab3_AllTasks")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab3_AllTasks")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("22222222-2222-2222-2222-222222222222")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
