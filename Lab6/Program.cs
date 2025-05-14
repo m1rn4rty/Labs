ed34f1c2427ef65914757f4bb95c3cf6a948f1b4
m1rn4rty <162566458+m1rn4rty@users.noreply.github.com>
2025-05-14 20:05:08 +0300

    Add files via upload
    Signed-off-by: m1rn4rty <162566458+m1rn4rty@users.noreply.github.com>

---
+++ Lab6/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Windows.Forms;
+
+namespace Lab6_AllTasks
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new FormPrint());
+        }
+    }
+}
---
+++ Lab6/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lab6_AllTasks")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab6_AllTasks")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("66666666-6666-6666-6666-666666666666")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
