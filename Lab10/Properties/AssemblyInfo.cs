7ac8f2444c262ee65a08559896e9df42e0f0b1ad
m1rn4rty <162566458+m1rn4rty@users.noreply.github.com>
2025-05-14 20:14:28 +0300

    Add files via upload
    Signed-off-by: m1rn4rty <162566458+m1rn4rty@users.noreply.github.com>

---
+++ Lab10/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lab10")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab10")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("10101010-1010-1010-1010-101010101010")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
